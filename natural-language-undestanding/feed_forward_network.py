5a0c3712392fa0be154703211481c89581783d0d
nityansuman <nityan.suman@gmail.com>
2021-07-11 23:01:32 +0530

    Adding ffn layer implementation

---
+++ natural-language-undestanding/feed_forward_network.py
@@ -0,0 +1,57 @@
+# Import packages
+import tensorflow as tf
+
+
+class FeedForwardNetworkLayer(tf.keras.layers.Layer):
+	"""Feed forward network layer implementation from transformers.
+
+	Args:
+		units (int, optional): Number of output units. Defaults to 2048.
+		activation (str, optional): Activation function to use. Defaults to "relu".
+		initializer (str, optional): Weight initializer to use. Defaults to "he_uniform".
+
+	Input shape:
+		N-D tensor of shape `batch_size, ..., input_dim`. An example of a 2D input of shape
+		`batch_size, input_dim`.
+
+	Output Shape:
+		N-D tensor of shape `batch_size, ..., units`. An example of a 2D output of shape
+		`batch_size, units`.
+	"""
+
+	def __init__(self, units=2048, activation="relu", initializer="he_uniform", **kwargs):
+		super().__init__(trainable=True, **kwargs)
+		self.units = units
+		self.initializer = initializer
+		self.activation = tf.keras.layers.Activation(activation=activation)
+
+	def build(self, input_shape):
+		self.w1 = self.add_weight(
+			name="w1",
+			shape=(input_shape[-1], self.units),
+			initializer=self.initializer,
+			trainable=True
+		)
+		self.b1 = self.add_weight(
+			name="b1",
+			shape=(self.units,),
+			initializer=tf.keras.initializers.RandomNormal(),
+			trainable=True
+		)
+		self.w2 = self.add_weight(
+			name="w2",
+			shape=[self.units, self.units],
+			initializer=self.initializer,
+			trainable=True
+		)
+		self.b2 = self.add_weight(
+			name="b2",
+			shape=(self.units,),
+			initializer=tf.keras.initializers.RandomNormal(),
+			trainable=True
+		)
+
+	def call(self, inputs):
+		x = tf.matmul(inputs, self.w1) + self.b1
+		x = self.activation(x)
+		return tf.matmul(x, self.w2) + self.b2
